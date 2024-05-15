49f87838c9de543a1ac9dde2c1fb037214711f63
CombatGoose <glebmikhov@gmail.com>
2024-05-15 19:16:07 +0300

    fix APP

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,4 @@
-import YourOrders from './Components/YourOrders/YourOrders';
-import PlaceAnOrder from './Components/PlaceAnOrder/PlaceAnorder';
-
 const App = () => {
-    return (
-        <YourOrders/>
-    )
 }
 
 export default App
