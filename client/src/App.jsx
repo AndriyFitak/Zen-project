e76f663bfff2ff636d779d84dc1bbd9b526d4694
CombatGoose <glebmikhov@gmail.com>
2024-06-01 09:56:06 +0300

    Add page information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Basket from '../src/Components/Basket/Basket'
 import MainPage from '../src/pages/main'
 import PlaceAnOrder from './Components/PlaceAnOrder/PlaceAnorder';
 import YourOrders from './Components/YourOrders/YourOrders';
+import Information from './Components/Information/Information';
 
 import { BrowserRouter, Routes, Route}  from"react-router-dom"
 
@@ -9,7 +10,9 @@ const App = () => {
     return (
         <>
         <BrowserRouter>
+
         <Routes>
+
             <Route path="/" element={
                 <MainPage/>
             }/>
@@ -21,11 +24,17 @@ const App = () => {
             <Route path="/placeAnOrder" element={
                 <PlaceAnOrder/>
             }/>
+
             <Route path="/yourOrders" element={
                 <YourOrders/>
             }/>
+
+            <Route path="/information" element={
+                <Information/>
+            }/>
             
         </Routes>
+
         </BrowserRouter>
         </>
     )
